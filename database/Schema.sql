78d1124a251cd8fb5f1ea95255cc9090d64a3393
Curtis Castro <curtisncastro@gmail.com>
2021-07-07 19:17:44 -0700

    seed dummy data to events table in psql

--- database/Schema.sql
+++ database/Schema.sql
@@ -5,18 +5,6 @@ CREATE TABLE Auth (
   username VARCHAR(500) NULL DEFAULT NULL,
   PRIMARY KEY (id)
 );
-
--- ---
--- INITIALIZE SERIALIZATION
--- ---
-
-SELECT setval(pg_get_serial_sequence('Auth', 'id'), coalesce(max(id)+1, 1), false) FROM Auth;
-
-INSERT INTO Auth(id, username)
-VALUES (1, 'test_user');
-
-
-
 -- ---
 -- Globals
 -- ---
@@ -29,19 +17,26 @@ VALUES (1, 'test_user');
 --
 -- ---
 
-DROP TABLE IF EXISTS `Events`;
+DROP TABLE IF EXISTS Events;
 
-CREATE TABLE `Events` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `gateway_id` INTEGER NULL DEFAULT NULL,
-  `driver_id` INTEGER NULL DEFAULT NULL,
-  `ext_cam` VARCHAR NULL DEFAULT NULL,
-  `int_cam` VARCHAR NULL DEFAULT NULL,
-  `acceleration` MEDIUMTEXT NULL DEFAULT NULL,
-  `incident_loc` MEDIUMTEXT NULL DEFAULT NULL,
-  `tags` MEDIUMTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE Events (
+  id SERIAL,
+  gateway_id INTEGER NULL DEFAULT NULL,
+  driver_id INTEGER NULL DEFAULT NULL,
+  ext_cam VARCHAR(500) NULL DEFAULT NULL,
+  int_cam VARCHAR(500) NULL DEFAULT NULL,
+  acceleration VARCHAR(500) NULL DEFAULT NULL,
+  incident_loc VARCHAR(500) NULL DEFAULT NULL,
+  tags VARCHAR(500) NULL DEFAULT NULL,
+  PRIMARY KEY (id)
 );
+
+-- ---
+-- INITIALIZE SERIALIZATION
+-- ---
+
+SELECT setval(pg_get_serial_sequence('Auth', 'id'), coalesce(max(id)+1, 1), false) FROM Auth;
+SELECT setval(pg_get_serial_sequence('Events', 'id'), coalesce(max(id)+1, 1), false) FROM Events;
 
 -- ---
 -- Table Properties
@@ -53,5 +48,13 @@ CREATE TABLE `Events` (
 -- Test Data
 -- ---
 
--- INSERT INTO `Events` (`id`,`gateway_id`,`driver_id`,`ext_cam`,`int_cam`,`acceleration`,`incident_loc`,`tags`) VALUES
--- ('','','','','','','','');
+INSERT INTO Events(id, gateway_id, driver_id, ext_cam, int_cam, acceleration, incident_loc, tags)
+VALUES (1001, 1,1,'http://www.imgurl.com/12345','http://www.imgurl.com/678910','{"speed_limit": 35, "acceleration": "[35,36,36,36,38,40,40,40,40,38,34,32,31,30,30,30,31,33,34,35,37]"}','51.3309754, 19.3744004','Harsh Turn');
+
+INSERT INTO Auth(id, username)
+VALUES (1, 'test_user');
+
+-- ---
+-- Seed Data
+-- ---
+COPY Events FROM '/Users/curtiscastro/Desktop/ValidAI.tr/repos/Version_0.1/database/Events.csv' DELIMITER ',' CSV HEADER;
